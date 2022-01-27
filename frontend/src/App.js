87d962516b81ff5b97fd7f3c85e110a97e85d535
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-01-27 22:11:23 +0900

    Design(postReview) : 리뷰 페이지 틀 구성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Header from './components/common/header/Header';
 import Post from './components/post-page/Post';
+import PostReview from './components/post-page/PostReview';
 import { Container } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  // state가 리뷰라면 리뷰게시판으로 이동, 리뷰게시판이 아닌 경우
+  // state가 개발 / 자유 입력으로 개발게시판과 자유게시판으로 나뉨.
+  const [isReview, setIsReview] = useState(true);
+  const [postName, setPostName] = useState('자유');
+
   return (
     <Container>
       <Header />
-      <Post />
+      {isReview ? <PostReview /> : <Post name={postName} />}
     </Container>
   );
 }
--- frontend/src/components/post-page/PostReview.jsx
+++ frontend/src/components/post-page/PostReview.jsx
@@ -7,9 +7,10 @@ import {
   TextField,
   Divider,
   Typography,
+  Rating,
 } from '@mui/material';
 
-function PostFree({ name }) {
+function PostFree() {
   return (
     <Container maxWidth="md">
       <TopTypography variant="h5">글 작성하기</TopTypography>
@@ -29,7 +30,7 @@ function PostFree({ name }) {
               fontWeight: 'bold',
             }}
           >
-            {name}게시판
+            리뷰게시판
           </Box>
           <TitleTextField
             required
@@ -39,18 +40,45 @@ function PostFree({ name }) {
             placeholder="제목을 입력하세요."
           />
         </TitleWrapper>
+        <ReviewPart>
+          <Box
+            sx={{
+              width: 200,
+              height: 200,
+              border: '1px solid black',
+              borderRadius: '8px',
+              lineHeight: '200px',
+              textAlign: 'center',
+            }}
+          >
+            이미지 공간
+          </Box>
+          <Typography>엘리스 : SW 엔지니어 트랙 1기</Typography>
+          <Rating name="reviewPoint" size="large" />
+          <Typography>별점을 선택해 주세요</Typography>
+        </ReviewPart>
         <ContentsWrapper>
           <ContentsTextField
             required
             type="text"
             fullWidth={true}
             multiline={true}
-            maxRows={10}
-            rows={10}
-            placeholder="내용을 입력하세요."
+            maxRows={5}
+            rows={3}
+            placeholder="장점을 입력하세요."
           />
         </ContentsWrapper>
-
+        <ContentsWrapper>
+          <ContentsTextField
+            required
+            type="text"
+            fullWidth={true}
+            multiline={true}
+            maxRows={5}
+            rows={3}
+            placeholder="단점을 입력하세요."
+          />
+        </ContentsWrapper>
         <SubmitButton variant="contained">등록</SubmitButton>
       </form>
     </Container>
@@ -67,14 +95,21 @@ const TopTypography = styled(Typography)`
 const TitleWrapper = styled.div`
   display: flex;
   flex-direction: row;
+  margin-bottom: 20px;
 `;
 
 const TitleTextField = styled(TextField)`
   margin: 10px;
 `;
 
+const ReviewPart = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const ContentsWrapper = styled.div`
-  margin: 10px;
+  margin: 40px 10px 10px 10px;
 `;
 
 const ContentsTextField = styled(TextField)`
