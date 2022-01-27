b98e96c5b9209d2c5197572a7efd83013c2436ec
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-01-27 17:21:23 +0900

    Design(Post) : 자유 / 개발 post페이지 디자인 완료

--- frontend/src/components/post-page/Post.jsx
+++ frontend/src/components/post-page/Post.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Button, Box, TextField } from '@mui/material';
+import {
+  Container,
+  Button,
+  Box,
+  TextField,
+  Divider,
+  Typography,
+} from '@mui/material';
 
 function PostFree() {
   return (
     <Container maxWidth="md">
+      <TopTypography variant="h5">글 작성하기</TopTypography>
+      <Divider></Divider>
       <form>
         <TitleWrapper>
           <Box
@@ -17,21 +26,32 @@ function PostFree() {
               lineHeight: '40px',
               margin: '10px',
               borderRadius: '8px',
+              fontWeight: 'bold',
             }}
           >
             자유게시판
           </Box>
           <TitleTextField
+            required
             type="text"
             size="small"
             fullWidth={true}
             placeholder="제목을 입력하세요."
           />
         </TitleWrapper>
-        <div className="contentWrapper">
-          <ContentsTextField type="text" />
-          <SubmitButton variant="contained">등록</SubmitButton>
-        </div>
+        <ContentsWrapper>
+          <ContentsTextField
+            required
+            type="text"
+            fullWidth={true}
+            multiline={true}
+            maxRows={10}
+            rows={10}
+            placeholder="내용을 입력하세요."
+          />
+        </ContentsWrapper>
+
+        <SubmitButton variant="contained">등록</SubmitButton>
       </form>
     </Container>
   );
@@ -39,10 +59,9 @@ function PostFree() {
 
 export default PostFree;
 
-const SubmitButton = styled(Button)`
-  && {
-    background-color: #a2d2ff;
-  }
+const TopTypography = styled(Typography)`
+  margin: 0 0 10px 10px;
+  font-weight: bold;
 `;
 
 const TitleWrapper = styled.div`
@@ -54,4 +73,17 @@ const TitleTextField = styled(TextField)`
   margin: 10px;
 `;
 
-const ContentsTextField = styled(TextField)``;
+const ContentsWrapper = styled.div`
+  margin: 10px;
+`;
+
+const ContentsTextField = styled(TextField)`
+  display: block;
+`;
+
+const SubmitButton = styled(Button)`
+  background-color: #a2d2ff;
+  position: relative;
+  left: 93%;
+  font-weight: bold;
+`;
