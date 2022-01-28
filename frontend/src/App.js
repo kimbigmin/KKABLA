6d798a9815df102fc3dbb8efb36705095a38ef57
qoeocjf5 <qoeocjf5@gmail.com>
2022-01-28 14:46:54 +0900

    feat(components/HotPostsBoard/HotPostsBoard.js): 추천 게시물 board 뼈대 container 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/common/header/Header';
 import { Container } from '@mui/material';
-
+import HotPostsBoard from './components/HotPostsBoard/HotPostsBoard';
 function App() {
   return (
     <Container>
       <Header />
+      <HotPostsBoard></HotPostsBoard>
     </Container>
   );
 }
