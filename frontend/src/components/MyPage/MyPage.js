b428eafec43e8f247fdcd44b48e40d5d900b70bc
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-02-03 14:12:53 +0900

    Design(mypage)  : 틀 구성

--- frontend/src/components/MyPage/MyPage.js
+++ frontend/src/components/MyPage/MyPage.js
@@ -1,34 +1,50 @@
-import { Container, Divider, Grid } from '@mui/material';
+import { Container, Divider, Grid, Box } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
-
 function MyPage() {
   return (
-    <Container maxWidth="md">
-      <Grid container spacing={4}>
+    <Container
+      sx={{
+        bgcolor: '#F7F7F7',
+      }}
+      maxWidth="md"
+    >
+      <Grid container spacing={3}>
         <Grid item xs={12}>
-          <GridSection>
-            <GridLabel>내 정보</GridLabel>
-            <Divider></Divider>
-          </GridSection>
+          <GridBox>
+            <GridSection>
+              <GridTitle>닉네임123 님</GridTitle>
+              <Divider></Divider>
+              <GridDetail></GridDetail>
+            </GridSection>
+          </GridBox>
         </Grid>
         <Grid item xs={4}>
-          <GridSection>
-            <GridLabel>내가 쓴 글</GridLabel>
-            <Divider></Divider>
-          </GridSection>
+          <GridBox>
+            <GridSection>
+              <GridTitle>작성한 글</GridTitle>
+              <Divider></Divider>
+              <GridDetail></GridDetail>
+            </GridSection>
+          </GridBox>
         </Grid>
         <Grid item xs={4}>
-          <GridSection>
-            <GridLabel>내가 쓴 리뷰</GridLabel>
-            <Divider></Divider>
-          </GridSection>
+          <GridBox>
+            <GridSection>
+              <GridTitle>작성한 리뷰</GridTitle>
+              <Divider></Divider>
+              <GridDetail></GridDetail>
+            </GridSection>
+          </GridBox>
         </Grid>
         <Grid item xs={4}>
-          <GridSection>
-            <GridLabel>좋아요</GridLabel>
-            <Divider></Divider>
-          </GridSection>
+          <GridBox>
+            <GridSection>
+              <GridTitle>좋아요</GridTitle>
+              <Divider></Divider>
+              <GridDetail></GridDetail>
+            </GridSection>
+          </GridBox>
         </Grid>
       </Grid>
     </Container>
@@ -41,6 +57,16 @@ const GridSection = styled.div`
   margin: 10px;
 `;
 
-const GridLabel = styled.label`
-  margin-bottom: 10px;
+const GridTitle = styled(Box)`
+  padding: 10px;
+  font-weight: 700;
+`;
+
+const GridBox = styled(Box)`
+  background-color: white;
+  border-radius: 15px;
+`;
+
+const GridDetail = styled(Box)`
+  min-height: 100px;
 `;
