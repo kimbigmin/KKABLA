1cd2a14ce97aff701998760b110fde5a39d96477
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-02-03 18:06:11 +0900

    Design(mypage) : 틀 구성 2

--- frontend/src/components/MyPage/MyPage.js
+++ frontend/src/components/MyPage/MyPage.js
@@ -1,4 +1,4 @@
-import { Container, Divider, Grid, Box } from '@mui/material';
+import { Container, Divider, Grid, Box, Rating } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
 function MyPage() {
@@ -19,21 +19,79 @@ function MyPage() {
             </GridSection>
           </GridBox>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={3.5}>
           <GridBox>
             <GridSection>
-              <GridTitle>작성한 글</GridTitle>
+              <GridTitle>작성한 글 n개</GridTitle>
               <Divider></Divider>
-              <GridDetail></GridDetail>
+              <GridDetail>
+                <GridDetailBox>
+                  <GridDetailTitle>
+                    글제목.....
+                    <Divider
+                      sx={{
+                        margin: '10px 0',
+                      }}
+                    ></Divider>
+                    글 내용 ...
+                  </GridDetailTitle>
+                </GridDetailBox>
+                <GridDetailBox>
+                  <GridDetailTitle>
+                    글제목.....
+                    <Divider
+                      sx={{
+                        margin: '10px 0',
+                      }}
+                    ></Divider>
+                    글 내용 ...
+                  </GridDetailTitle>
+                </GridDetailBox>
+                <GridDetailBox>
+                  <GridDetailTitle>
+                    글제목.....
+                    <Divider
+                      sx={{
+                        margin: '10px 0',
+                      }}
+                    ></Divider>
+                    글 내용 ...
+                  </GridDetailTitle>
+                </GridDetailBox>
+              </GridDetail>
             </GridSection>
           </GridBox>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4.5}>
           <GridBox>
             <GridSection>
-              <GridTitle>작성한 리뷰</GridTitle>
-              <Divider></Divider>
-              <GridDetail></GridDetail>
+              <GridTitle>작성한 리뷰 n개</GridTitle>
+              <Divider
+                sx={{
+                  margin: '10px 0',
+                }}
+              ></Divider>
+              <GridDetail>
+                <GridDetailBox>
+                  <Grid container>
+                    <Grid item xs={4}>
+                      <RatingBox>
+                        3.0
+                        <Rating
+                          name="read-only"
+                          value={3}
+                          size="small"
+                          readOnly
+                        />
+                      </RatingBox>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <RatingDate>2022-02-03</RatingDate>
+                      <RatingName>Elice SW Engineer Track 1기</RatingName>
+                    </Grid>
+                  </Grid>
+                </GridDetailBox>
+              </GridDetail>
             </GridSection>
           </GridBox>
         </Grid>
@@ -54,7 +112,7 @@ function MyPage() {
 export default MyPage;
 
 const GridSection = styled.div`
-  margin: 10px;
+  padding: 10px;
 `;
 
 const GridTitle = styled(Box)`
@@ -65,8 +123,40 @@ const GridTitle = styled(Box)`
 const GridBox = styled(Box)`
   background-color: white;
   border-radius: 15px;
+  margin: 10px;
 `;
 
 const GridDetail = styled(Box)`
   min-height: 100px;
+`;
+
+const GridDetailBox = styled(Box)`
+  background-color: #f7f7f7;
+  border-radius: 10px;
+  position: relative;
+`;
+
+const GridDetailTitle = styled(Box)`
+  margin: 10px;
+  font-size: 16px;
+  padding: 10px;
+  font-weight: 500;
+`;
+
+const RatingBox = styled(Box)`
+  text-align: center;
+  margin: 26px 0;
+`;
+
+const RatingDate = styled(Box)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 5px;
+  font-size: 12px;
+`;
+
+const RatingName = styled(Box)`
+  padding: 26px 10px;
+  text-align: justify;
 `;
