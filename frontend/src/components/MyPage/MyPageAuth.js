52ad806c036c2dc9a597e31180ea7214e6703458
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-02-04 19:22:19 +0900

    Design(Mypage) :  인증된 경우 / 아닌경우 제작 1

--- frontend/src/components/MyPage/MyPageAuth.js
+++ frontend/src/components/MyPage/MyPageAuth.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState, Children } from 'react';
+import { Box, Container } from '@mui/material';
+import styled from 'styled-components';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 
 function MyPageAuth({ content }) {
-  return <h1>인증하기</h1>;
+  const [auth, setAuth] = useState(true);
+
+  return (
+    <AuthContainer>
+      {auth === false ? (
+        <>
+          {Children.toArray(
+            content.map((el) => (
+              <AuthBox>
+                {el.part} 수료생
+                <EmojiEventsIcon />
+              </AuthBox>
+            )),
+          )}
+        </>
+      ) : (
+        <AuthNeedBox>인증하기</AuthNeedBox>
+      )}
+    </AuthContainer>
+  );
 }
 
+// 데이터가 여러개로 들어오면 map으로 구성하는 방법을 고려,
+
 export default MyPageAuth;
+
+const AuthContainer = styled(Container)`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const AuthBox = styled(Box)`
+  margin: 10px;
+  background-color: #bde7ff;
+  width: 180px;
+  height: 60px;
+  padding: 10px;
+  border-radius: 20px;
+  text-align: justify;
+`;
+
+const AuthNeedBox = styled(Box)`
+  margin: 10px;
+  background-color: #bde7ff;
+  width: 180px;
+  height: 60px;
+  padding: 10px;
+  border-radius: 20px;
+  text-align: center;
+  line-height: 60px;
+  font-size: 20px;
+`;
