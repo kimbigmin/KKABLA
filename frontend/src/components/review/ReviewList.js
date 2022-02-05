737b3980be3fa96aa563baf1bf25d80b9fdf7afc
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-02-06 04:06:14 +0900

    Add(src/components/review/ReviewList.js): 더미데이터 바인딩 및 사용자 익명기능 추가

--- frontend/src/components/review/ReviewList.js
+++ frontend/src/components/review/ReviewList.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import StarIcon from '@mui/icons-material/Star';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
 import styled from 'styled-components';
+import { getStars } from './util/getStars';
 
-function ReviewList({ isLogin }) {
+function ReviewList({ isLogin, review }) {
+  const anonymousName = review.creator[0] + '*'.repeat(6);
+
+  // Star 아이콘 렌더링 함수
+
   return (
     <List>
       <Score>
-        <span>
-          {/* 받아온 평점으로 동적으로 별 생성하기 */}
-          <StarIcon sx={{ color: '#fcdd29', fontSize: '1rem' }} />
-          <StarIcon sx={{ color: '#fcdd29', fontSize: '1rem' }} />
-          <StarIcon sx={{ color: '#fcdd29', fontSize: '1rem' }} />
-          <StarHalfIcon sx={{ color: '#fcdd29', fontSize: '1rem' }} />
-        </span>
-        <p>{3.5}점</p>
+        <span>{getStars(review.star)}</span>
+        <p>{review.star}점</p>
       </Score>
       <Contents>
         <p className="writer">
-          작성자 : <span>{'i*****'}</span>
+          작성자 : <span>{anonymousName}</span>
           <span className="auth">{'수강생인증'}</span>
         </p>
         <p className="content">
           <h4 className="title">장점</h4>
           <div>
             {isLogin
-              ? '로그인시 보이는 내용 로그인시 보이는 내용 로그인시 보이는 내용 로그인시 보이는 내용 로그인시 보이는 내용'
+              ? `${review.pros}`
               : '로그인을 해주세요. 로그인을 해주세요. 로그인을 해주세요. 로그인을 해주세요.로그인을 해주세요. 로그인을 해주세요. (비회원일 경우 보이는 내용)'}
           </div>
         </p>
@@ -33,12 +30,12 @@ function ReviewList({ isLogin }) {
           <h4 className="title">단점</h4>
           <div>
             {isLogin
-              ? '로그인시 보이는 내용 로그인시 보이는 내용 로그인시 보이는 내용 로그인시 보이는 내용 로그인시 보이는 내용'
+              ? `${review.cons}`
               : '로그인을 해주세요. 로그인을 해주세요. 로그인을 해주세요. 로그인을 해주세요.로그인을 해주세요. 로그인을 해주세요. (비회원일 경우 보이는 내용)'}
           </div>
         </p>
       </Contents>
-      <Date>{'2022.01.26'}</Date>
+      <Date>{review.timeStamp}</Date>
     </List>
   );
 }
