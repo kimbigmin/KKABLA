e0a9d3423c224deb4f0958d34f69eda9ffaa7726
clsrns111 <clsrns1111@gmail.com>
2022-02-05 15:43:35 +0900

    머지

---
+++ backend/routes/home.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import Board from '../models/Board.js';
+import BootCamp from '../models/BootCamp.js';
+import Review from '../models/Review.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const boards = await Board.find({});
+  const bootCamps = await BootCamp.find({});
+  const reviews = await Review.find({});
+
+  res.send({ boards, bootCamps, reviews });
+});
+
+export default rotuer;
