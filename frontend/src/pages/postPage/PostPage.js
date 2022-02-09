e70731e5d5d38c4cd370ea5476d96a40f63adc90
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-02-09 17:14:30 +0900

    feat(post, postReview) : 등록시 핸들러 추가 , navigate는 아직

--- frontend/src/pages/postPage/PostPage.js
+++ frontend/src/pages/postPage/PostPage.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Container, Typography, Divider } from '@mui/material';
 import Post from '../../components/post-page/Post';
 import PostReview from '../../components/post-page/PostReview';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function PostPage({ isLogin }) {
+  const [post, setPost] = useState({});
+
   // param으로 전달 된 board 따라서 게시판이 변경됨
   const param = useParams();
   const board = param.board;
+
+  useEffect(() => {
+    function getPost() {
+      axios
+        .get(`http://localhost:5000/board/${board}`)
+        .then((res) => setPost(res.data));
+    }
+
+    getPost();
+  }, []);
+
+  console.log(post);
 
   return (
     <PostContainer>
