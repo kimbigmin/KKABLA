18fb1fc9eafea541d6e62ebf0fd874818029aba7
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2022-02-12 12:42:30 +0900

    feat(mypage/auth) : 인증에 부트캠프명 입력 필드 추가

--- frontend/src/components/MyPage/AuthPageTitle.js
+++ frontend/src/components/MyPage/AuthPageTitle.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, TextField } from '@mui/material';
 import styled from 'styled-components';
 
 function AuthPageTitle() {
-  return <TitleBox>수료증을 업로드해 주세요 !</TitleBox>;
+  return (
+    <>
+      <NameInputBox>
+        <NameBox>부트캠프 이름</NameBox>
+        <TitleTextField
+          required
+          type="text"
+          size="small"
+          fullWidth={true}
+          placeholder="제목을 입력하세요."
+        />
+      </NameInputBox>
+      <TitleBox>수료증을 업로드해 주세요 !</TitleBox>
+    </>
+  );
 }
 export default AuthPageTitle;
 
@@ -16,4 +30,27 @@ const TitleBox = styled(Box)`
   background-color: #e8e8e8;
   font-weight: bold;
   margin: auto;
+`;
+
+const TitleTextField = styled(TextField)`
+  margin: 10px;
+`;
+
+const NameInputBox = styled(Box)`
+  display: flex;
+  width: 60%;
+  margin: auto;
+`;
+
+const NameBox = styled(Box)`
+  width: 200px;
+  height: 40px;
+  background-color: #a2d2ff;
+  color: black;
+  text-align: center;
+  line-height: 40px;
+  margin: 10px;
+  border-radius: 8px;
+  font-weight: bold;
+  margin-bottom: 40px;
 `;
