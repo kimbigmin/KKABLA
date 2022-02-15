4d926d51322fda78261ac4bd439762fb79ce09d6
qoeocjf5 <qoeocjf5@gmail.com>
2022-02-15 17:56:37 +0900

    merge 전 그냥 커밋

--- frontend/src/components/mainPage/MainContents.js
+++ frontend/src/components/mainPage/MainContents.js
@@ -6,38 +6,6 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function MainContents({ isLogin }) {
-  // const [freeBoard, setFreeBoard] = useState([]);
-  // const [developBoard, setDevelopBoard] = useState([]);
-  // const [hotBootCamps, setHotBootCamps] = useState([]);
-  // const [hotPostsBoard, setHotPostsBoard]=useState([]);
-
-  // useEffect(()=>{
-    //   const postReq = async () => {
-      //     await axios
-      //     .get('http://localhost:5000/', { withCredentials: true })
-      //     .then((res) => {
-        //         // console.log(res.data)
-        //         setTotalBoards(res.data);
-        
-        //         // setFreeBoard((prevState) =>{ 
-          //         //   return [...res.data.boards];      
-          //         // });
-          //         // setDevelopBoard((prevState) =>{
-            //         //   return [...res.data.develop];      
-            //         // });
-            //         // setHotPostsBoard((prevState)=>{
-              //         //   return [...res.data.like];
-              //         // });
-              //         setHotBootCamps((prevState)=>{
-                //           return [...res.data.bootCamps];
-                //         });
-                //       })
-                //       .catch((err) => console.log(err));
-                //   };
-                
-                //   postReq();
-                // },[])  
-                
   const [totalBoards,setTotalBoards]=useState();
   
   useEffect(()=>{
