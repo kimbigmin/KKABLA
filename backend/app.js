0d92aeadf06dedb730f4270dcae9b491e590e39d
clsrns111 <clsrns1111@gmail.com>
2022-02-19 14:10:48 +0900

    fix(comment boardType)

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'http://elice-kdt-sw-1st-team10.elicecoding.com',
   }),
 );
 
